31e3843319be464e0bd29f4fceee7e2dd2e1f551
wecode <umurangamclaire@gmail.com>
2019-02-27 18:43:48 +0200

    Added macro to index

--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
-sender_email = 'nancytoniw@gmail.com'
+sender_email = 'angelakarenzi5@gmail.com'
 
 def mail_message(subject,template,to,**kwargs):
    
